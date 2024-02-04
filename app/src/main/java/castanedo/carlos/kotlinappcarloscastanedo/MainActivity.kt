f99d7e7fb6b61a08cd4dfa2eb9c6a746da9af38b
cxrlitxs <carloscastanedo.88@gmail.com>
2024-02-04 18:50:33 +0100

    Created FrameLayout and changed its color by parameter

--- app/src/main/java/castanedo/carlos/kotlinappcarloscastanedo/MainActivity.kt
+++ app/src/main/java/castanedo/carlos/kotlinappcarloscastanedo/MainActivity.kt
@@ -1,11 +1,24 @@
 package castanedo.carlos.kotlinappcarloscastanedo
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Referencia a la vista
+        val bodyLayout = findViewById<FrameLayout>(R.id.bodyLayout)
+
+        // Cambia el color de fondo
+        frameLayoutSetBackgroudColor(bodyLayout, Color.rgb(169 ,251,230)) // Puedes cambiar el color aquí
+
+    }
+
+    private fun frameLayoutSetBackgroudColor(frameLayout: FrameLayout, color: Int) {
+        frameLayout.setBackgroundColor(color)
     }
 }
