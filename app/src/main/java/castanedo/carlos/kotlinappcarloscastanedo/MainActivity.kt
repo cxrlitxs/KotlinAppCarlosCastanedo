ffc95032b0afd91e6ad4a6f83c1a9ced502662a1
cxrlitxs <carloscastanedo.88@gmail.com>
2024-02-04 23:10:26 +0100

    Implemented button to change the color of the FrameLayout

--- app/src/main/java/castanedo/carlos/kotlinappcarloscastanedo/MainActivity.kt
+++ app/src/main/java/castanedo/carlos/kotlinappcarloscastanedo/MainActivity.kt
@@ -1,24 +1,45 @@
 package castanedo.carlos.kotlinappcarloscastanedo
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import android.widget.FrameLayout
+import kotlin.random.Random
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    lateinit var bodyLayout:FrameLayout
+    lateinit var buttonChangeColor:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Referencia a la vista
-        val bodyLayout = findViewById<FrameLayout>(R.id.bodyLayout)
-
+        bodyLayout = findViewById<FrameLayout>(R.id.bodyLayout)
         // Cambia el color de fondo
         frameLayoutSetBackgroudColor(bodyLayout, Color.rgb(169 ,251,230)) // Puedes cambiar el color aquí
+
+
+        buttonChangeColor=findViewById(R.id.buttonChangeColor)
+
+        buttonChangeColor.setOnClickListener(this)
 
     }
 
     private fun frameLayoutSetBackgroudColor(frameLayout: FrameLayout, color: Int) {
         frameLayout.setBackgroundColor(color)
     }
+
+    override fun onClick(v: View?) {
+        if(v!!.id==R.id.buttonChangeColor){
+            val randomColor = Color.argb(255, Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+            bodyLayout.setBackgroundColor(randomColor)
+        }
+    }
+
 }
