0892dcfe8c75c8812e5c9f2d964f23a103d78016
cxrlitxs <carloscastanedo.88@gmail.com>
2024-02-06 02:26:40 +0100

    Implemented the login base with authentication in Firebase working

--- app/src/main/java/castanedo/carlos/kotlinappcarloscastanedo/OnBoarding.kt
+++ app/src/main/java/castanedo/carlos/kotlinappcarloscastanedo/OnBoarding.kt
@@ -2,10 +2,28 @@ package castanedo.carlos.kotlinappcarloscastanedo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class OnBoarding : AppCompatActivity() {
+class OnBoarding : AppCompatActivity(), OnClickListener{
+
+    private lateinit var navController:NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding)
+        navController = findNavController(R.id.nav_host_fragment_content_main)
     }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+
 }
--- app/src/main/java/castanedo/carlos/kotlinappcarloscastanedo/fragments/LoginFragment.kt
+++ app/src/main/java/castanedo/carlos/kotlinappcarloscastanedo/fragments/LoginFragment.kt
@@ -1,33 +1,34 @@
 package castanedo.carlos.kotlinappcarloscastanedo.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.NavController
+import castanedo.carlos.kotlinappcarloscastanedo.MainActivity
 import castanedo.carlos.kotlinappcarloscastanedo.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class LoginFragment : Fragment(), OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var auth: FirebaseAuth
+    private lateinit var navController: NavController
+    lateinit var edTxtEmail:EditText
+    lateinit var edTxtPassword:EditText
+    lateinit var loginButton:Button
+    lateinit var registerButton:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +36,58 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        auth = Firebase.auth
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        edTxtEmail = view.findViewById(R.id.edTxtEmail)
+        edTxtPassword = view.findViewById(R.id.edTxtPassword)
+        loginButton = view.findViewById(R.id.loginButton)
+        registerButton = view.findViewById(R.id.registerButton)
+
+        loginButton.setOnClickListener(this)
+        registerButton.setOnClickListener(this)
+
     }
+
+    override fun onClick(v: View?) {
+        if (v!!.id == R.id.loginButton){
+            val sEmail = edTxtEmail.text.toString()
+            val sPassword = edTxtPassword.text.toString()
+
+            if (sEmail.isEmpty() || sPassword.isEmpty()) {
+                Toast.makeText(
+                    requireActivity(),
+                    "Authentication failed.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+                return
+            }
+
+            auth.signInWithEmailAndPassword(sEmail, sPassword)
+                .addOnCompleteListener(requireActivity()) { task ->
+
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        var mainActivity: Intent = Intent(requireActivity(), MainActivity::class.java)
+                        startActivity(mainActivity)
+                        requireActivity().finish()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            requireActivity(),
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+
+        } else if(v!!.id == R.id.registerButton){
+            navController.navigate(R.id.action_loginFragment_to_registerFragment2)
+        }
+    }
+
+
 }
